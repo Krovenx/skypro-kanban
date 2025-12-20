212866332923545628f7cbba43fa010b30b42c15
Krovenx <Krovenx2@mail.ru>
2025-12-20 15:21:30 +0500

    Созданы папки с файлам scss и jsx всё подключил и настроил

---
+++ src/Components/Column/Column.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Column.scss';
+
+const Column = ({ title, children }) => {
+  return (
+    <div className="main__column">
+      <div className="column__title">
+        <p>{title}</p>
+      </div>
+      <div className="cards">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Column;
---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Main.scss";
+
+const Main = ({ children }) => {
+  return (
+    <main className="main">
+      <div className="container">
+        <div className="main__block">
+          <div className="main__content">{children}</div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Main;
